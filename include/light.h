2257d311d01747e23fbb88216d19064570f7a1a5
ZhuLei <zhulei@xmfunny.com>
2022-06-07 16:56:22 +0800

    <feature> add sub lib

---
+++ include/light.h
@@ -0,0 +1,13 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+class light
+{
+private:
+    /* data */
+public:
+    light(/* args */);
+    ~light();
+};
+
+#endif
---
+++ src/light.cpp
@@ -0,0 +1,12 @@
+#include "light.h"
+#include <memory.h>
+
+light::light(/* args */)
+{
+}
+
+light::~light()
+{
+
+
+}
