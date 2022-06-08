a1dd66a86f3ff3cac4cfed397fadacd470571717
ZhuLei <zhulei@xmfunny.com>
2022-06-08 20:02:07 +0800

    <feature> show window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello world!" << std::endl;
-    system("Pause");
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Title", NULL, NULL);
+    if (window == NULL)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+
 }
