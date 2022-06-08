b6c979eedcca9a940cc5c56ed90a6ec7be11f744
ZhuLei <zhulei@xmfunny.com>
2022-06-08 20:29:25 +0800

    <feature> add glad

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 int main(int argc, char* argv[])
 {
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Title", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "MyGraphics_OpenGL", NULL, NULL);
     if (window == NULL)
     {
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to init GLAD." << std::endl;
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window))
     {
         glfwSwapBuffers(window);
